a9570b45df8f9bc0fdf9a46cd1656563c95f740c
ultrAlper23 <alper.kalaycioglu@gmail.com>
2016-08-31 16:53:51 +0300

    Version1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace UrunGuncelleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.gvProducts = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gvProducts
+            // 
+            this.gvProducts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gvProducts.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.gvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvProducts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gvProducts.Location = new System.Drawing.Point(0, 0);
+            this.gvProducts.Name = "gvProducts";
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gvProducts.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.gvProducts.Size = new System.Drawing.Size(838, 211);
+            this.gvProducts.TabIndex = 0;
+            this.gvProducts.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvProducts_CellValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 337);
+            this.Controls.Add(this.gvProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvProducts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gvProducts;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UrunGuncelleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("server=.; database=northwnd; integrated security=true");
+        DataTable dt = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("select top 10 ProductID, ProductName, UnitPrice, CategoryID from Products order by newID() ", con);
+
+            adp.Fill(dt);
+            gvProducts.DataSource = dt;
+            gvProducts.Columns["ProductID"].Visible = false;
+          
+        }
+
+        private void gvProducts_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = Convert.ToInt32(dt.Rows[e.RowIndex]["ProductID"]);
+
+            int kolon = e.ColumnIndex;
+            string kolonAdi = gvProducts.Columns[kolon].Name;//hangi kolonda değişiklik yapılmış
+
+            SqlCommand update = new SqlCommand(string.Format("update products set {0}=@p where ProductID=@id", kolonAdi), con);//set işlemi kolon adına göre yapılacak.
+            
+            update.Parameters.AddWithValue("@p",
+                gvProducts.CurrentCell.Value);//geçerli olan hücrenin değerini aktar.
+
+            update.Parameters.AddWithValue("@id", id);
+            con.Open();
+            update.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
